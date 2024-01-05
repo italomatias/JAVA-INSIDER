00e3b6e7199d4ba727b4c903f607d98dc43ea4dc
Italo Reis <italomatiasdosreis@gmail.com>
2024-01-04 23:34:45 -0300

    Exercício para converter Fahrenheit para CELSIUS

---
+++ CONVERSOR FH para Celsius/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double TF = 84.5;
+
+        double TC = (TF - 32) / 9.0 * 5;
+
+        System.out.println("A temperaduta : " + TF + " Fahrenheit corresponde a : " + TC + " CELSIUS." );
+
+    }
+}
